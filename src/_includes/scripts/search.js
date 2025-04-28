71858c9d063d760f1eb3db112db309119900f106
= <=>
2025-04-28 07:28:50 -0700

    make filtering more intuitive

--- src/_includes/scripts/search.js
+++ src/_includes/scripts/search.js
@@ -45,7 +45,7 @@ async function setupSearch() {
   const bookList = document.getElementById('book-list');
 
   function renderResults(results) {
-    console.log("results len = " + results.length);
+    console.debug("results len = " + results.length);
 
     if (bookList == null || bookList.innerHTML == null) {
       document.getElementById("no-results").style.display = "block";
@@ -54,41 +54,49 @@ async function setupSearch() {
     else {
       let cards = document.getElementsByClassName("book-card");
 
-      if (results.length === 0) {
+      if (results.length === 0) { 
+      // filtered out all the books
         document.getElementById("no-results").style.display = "block";
-        console.log(`cards len = ${cards.length}`);
+        console.debug(`cards len = ${cards.length}`);
         for (const card of cards) {
           card.style.display = "none";
         }
-      } else {
+      } else { // we have filtered some books
+        // hide the no results message 
         document.getElementById("no-results").style.display = "none";
-
+        // and for all the cards set the display attribute
+        // none is the default
+        // block if it is in the search result
         for (const card of cards) {
           const hit = results.find(b => b.ref === card.title);
           if (hit != undefined) {
+            console.debug(`ref = ${hit.ref} card = ${card.ariaLabel}`);
             card.style.display = "block";
+          } else {
+            card.style.display = "none";
           }
         }
-//         bookList.innerHTML = results.map(result => {
-//           const book = books.find(b => b.url === result.ref);
-//           return `<a href="${book.url}"><img src="${book.data.img_url}" alt="${book.data.title} by ${book.data.author}" />
-// </a>`;
-//         }).join('');
-
       }
-     // reinitializeImages();
 
     }
   }
 
   searchBox.addEventListener('input', function () {
-    const query = this.value.trim();
+
+    // Append an asterisk to the query if necessary because that makes the search fuzzy.
+    // Without this, all books are filtered (showing nothing) until an exact hit OR the user adds the asterisk.
+    // And that behavior is confusing/annoying/bad.
+    // If we replace lunr with something else then we can revisit.
+    var query = this.value.trim();
 
     if (query.length === 0) {
       renderResults(books.map(book => ({ ref: book.url })));
       return;
     }
-
+    if (query.endsWith('*') == false){
+      query = query+'*';
+    }
+    console.debug(`qry = ${query}`);
     const results = idx.search(query);
     renderResults(results);
   });
